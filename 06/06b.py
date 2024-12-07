8e401f5a485be247204868da795c25dab9c52551
Rob Hailman <robhailman@gmail.com>
2024-12-07 18:30:20 -0500

    Refactor day 6
    Rather than checking entire grid, only check locations that unmodified path travels through

--- 06/06b.py
+++ 06/06b.py
@@ -44,45 +44,64 @@ if __name__ == "__main__":
 
     start_position = GUARD_POSITION
 
-    print(X_SIZE, Y_SIZE, X_SIZE * Y_SIZE)
+    # do simple walk through path (from part 1) to build list of potential wall locations
+    potential_wall_locations = []
+    while valid_position(GUARD_POSITION):
+        candidate_location = (
+            GUARD_POSITION[0] + MOVEMENT_VECTORS[GUARD_DIRECTION][0],
+            GUARD_POSITION[1] + MOVEMENT_VECTORS[GUARD_DIRECTION][1],
+        )
+        if candidate_location in WALL_LOCATIONS:
+            GUARD_DIRECTION += 1
+            GUARD_DIRECTION %= 4
+        else:
+            if GUARD_POSITION not in potential_wall_locations:
+                potential_wall_locations.append(GUARD_POSITION)
+            GUARD_POSITION = candidate_location
+
+    print(len(potential_wall_locations))
+    iter = 0
 
     # brute force because I'm not clever
-    for x in range(X_SIZE):
-        print(x, end=" ")
-        for y in range(Y_SIZE):
-            if y % 5 == 0:
-                print(".", end="", flush=True)
-            if (x, y) != start_position and (x, y) not in WALL_LOCATIONS:
-                # add this location to local list of wall locations
-                iter_wall_locations = WALL_LOCATIONS.copy()
-                iter_wall_locations.append((x, y))
-                done_iter = False
+    # rather than checking every location, only places that unmodified path travels through
+    for loc in potential_wall_locations:
+        iter += 1
+        if iter % 10 == 0:
+            print(".", end="", flush=True)
+        if iter % 500 == 0:
+            print("", score, "(", iter, "/", len(potential_wall_locations), ")")
 
-                # reset path
-                GUARD_DIRECTION = 0
-                GUARD_POSITION = start_position
-                VISITED_LOCATIONS = defaultdict(list)
-                while not done_iter:
-                    if not valid_position(GUARD_POSITION):
-                        # print(x, y, " escaped")
+        if loc != start_position and loc not in WALL_LOCATIONS:
+            # add this location to local list of wall locations
+            iter_wall_locations = WALL_LOCATIONS.copy()
+            iter_wall_locations.append(loc)
+            done_iter = False
+
+            # reset path
+            GUARD_DIRECTION = 0
+            GUARD_POSITION = start_position
+            VISITED_LOCATIONS = defaultdict(list)
+            while not done_iter:
+                if not valid_position(GUARD_POSITION):
+                    # print(x, y, " escaped")
+                    done_iter = True
+                else:
+                    candidate_location = (
+                        GUARD_POSITION[0] + MOVEMENT_VECTORS[GUARD_DIRECTION][0],
+                        GUARD_POSITION[1] + MOVEMENT_VECTORS[GUARD_DIRECTION][1],
+                    )
+                    if candidate_location in iter_wall_locations:
+                        GUARD_DIRECTION += 1
+                        GUARD_DIRECTION %= 4
+                    elif GUARD_DIRECTION in VISITED_LOCATIONS[GUARD_POSITION]:
+                        # print(x, y, " caught in loop")
+                        score += 1
                         done_iter = True
                     else:
-                        candidate_location = (
-                            GUARD_POSITION[0] + MOVEMENT_VECTORS[GUARD_DIRECTION][0],
-                            GUARD_POSITION[1] + MOVEMENT_VECTORS[GUARD_DIRECTION][1],
-                        )
-                        if candidate_location in iter_wall_locations:
-                            GUARD_DIRECTION += 1
-                            GUARD_DIRECTION %= 4
-                        elif GUARD_DIRECTION in VISITED_LOCATIONS[GUARD_POSITION]:
-                            # print(x, y, " caught in loop")
-                            score += 1
-                            done_iter = True
-                        else:
-                            VISITED_LOCATIONS[GUARD_POSITION].append(GUARD_DIRECTION)
-                            GUARD_POSITION = candidate_location
-        print(score)
+                        VISITED_LOCATIONS[GUARD_POSITION].append(GUARD_DIRECTION)
+                        GUARD_POSITION = candidate_location
 
+    print()
     print(score)
     end_time = perf_counter()
     print(f"Execution time: {end_time-start_time:.3f}s")
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Collection of my solutions for Advent of Code 2024. Unless I feel like doing som
 | 3    | Dec 5, 12:09p  | 0.001s | 0.008s |  |
 | 4    | Dec 6, 5:54p   | 3.289s | 2.505s |  |
 | 5    | Dec 6, 6:19p   | 0.003s | 0.180s |  |
-| 6    | Dec 7, 4:04p   | 0.217s | 648.1s |  |
+| 6    | Dec 7, 4:04p   | 0.217s | *142.9s* | Dec 7, 6:11p |
 | 7    | Dec 7, 4:51p   | 0.308s | 23.56s |  |
 
 Completion/refactor timestamps `tz=America/Toronto`. Execution times in *italics* indicate a significant improvement from refactoring after the puzzle was completed and submitted.
